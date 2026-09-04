db2a4ba96d0a236f21bfd6c03d041711096a7be9
mynamenoturs-cmd <mynamenoturs@gmail.com>
2026-09-05 01:26:48 +0800

    chore: load BM Year 3 units 1-3 source blueprints

--- app-v03334.js
+++ app-v03334.js
@@ -21,6 +21,7 @@ document.write('<script src="rph-bm-year2-units13-15-blueprint-hotfix.js?v=20260
 document.write('<script src="rph-bm-year2-units16-18-blueprint-hotfix.js?v=20260904a"><\/script>');
 document.write('<script src="rph-bm-year2-units19-21-blueprint-hotfix.js?v=20260904a"><\/script>');
 document.write('<script src="rph-bm-year2-units22-24-blueprint-hotfix.js?v=20260905a"><\/script>');
+document.write('<script src="rph-bm-year3-units1-3-blueprint-hotfix.js?v=20260905a"><\/script>');
 document.write('<script src="rph-science-quality-hotfix.js?v=20260904a"><\/script>');
 document.write('<script src="rph-science-blueprint-hotfix.js?v=20260904a"><\/script>');
 document.write('<script src="rph-science-magnet-actions-blueprint-hotfix.js?v=20260904b"><\/script>');
