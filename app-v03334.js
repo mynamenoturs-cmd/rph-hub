d25d47a69553521ebe9db3a230cb1191c1d18031
mynamenoturs-cmd <mynamenoturs@gmail.com>
2026-09-04 11:51:06 +0800

    chore: load Science source blueprint after quality repair

--- app-v03334.js
+++ app-v03334.js
@@ -5,4 +5,5 @@ document.write('<script src="rph-week-routing-hotfix.js?v=20260904b"><\/script>'
 document.write('<script src="lessonmap-review-hotfix.js?v=20260904a"><\/script>');
 document.write('<script src="rph-quality-hotfix.js?v=20260904a"><\/script>');
 document.write('<script src="rph-science-quality-hotfix.js?v=20260904a"><\/script>');
+document.write('<script src="rph-science-blueprint-hotfix.js?v=20260904a"><\/script>');
 document.write('<script src="rph-english-quality-hotfix.js?v=20260904a"><\/script>');
