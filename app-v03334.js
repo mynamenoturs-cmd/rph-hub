1f62ae70065c6ce228bff408a0758c131c3487b7
mynamenoturs-cmd <mynamenoturs@gmail.com>
2026-09-04 13:08:40 +0800

    chore: load Year 1 magnet action blueprints

--- app-v03334.js
+++ app-v03334.js
@@ -6,6 +6,7 @@ document.write('<script src="lessonmap-review-hotfix.js?v=20260904a"><\/script>'
 document.write('<script src="rph-quality-hotfix.js?v=20260904a"><\/script>');
 document.write('<script src="rph-science-quality-hotfix.js?v=20260904a"><\/script>');
 document.write('<script src="rph-science-blueprint-hotfix.js?v=20260904a"><\/script>');
+document.write('<script src="rph-science-magnet-actions-blueprint-hotfix.js?v=20260904b"><\/script>');
 document.write('<script src="rph-science-magnet-blueprint-hotfix.js?v=20260904a"><\/script>');
 document.write('<script src="rph-science-absorption-blueprint-hotfix.js?v=20260904a"><\/script>');
 document.write('<script src="rph-english-quality-hotfix.js?v=20260904a"><\/script>');
