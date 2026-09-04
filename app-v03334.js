84404a5e2b73760c5d076361fe4a385e27ca8eb1
mynamenoturs-cmd <mynamenoturs@gmail.com>
2026-09-04 16:08:55 +0800

    chore: load Year 3 Science unit source blueprints

--- app-v03334.js
+++ app-v03334.js
@@ -15,4 +15,5 @@ document.write('<script src="rph-science-earth-soil-blueprint-hotfix.js?v=202609
 document.write('<script src="rph-science-year2-water-air-blueprint-hotfix.js?v=20260904a"><\/script>');
 document.write('<script src="rph-science-year2-technology-blueprint-hotfix.js?v=20260904a"><\/script>');
 document.write('<script src="rph-science-year3-source-blueprint-hotfix.js?v=20260904a"><\/script>');
+document.write('<script src="rph-science-year3-future-blueprint-hotfix.js?v=20260904a"><\/script>');
 document.write('<script src="rph-english-quality-hotfix.js?v=20260904a"><\/script>');
