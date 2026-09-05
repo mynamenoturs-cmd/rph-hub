563f5682fc575a7c7cf123acda8a38be08dd0e4a
mynamenoturs-cmd <mynamenoturs@gmail.com>
2026-09-05 11:47:48 +0800

    chore: load English Year 2 Unit 8 source blueprint

--- app-v03334.js
+++ app-v03334.js
@@ -44,4 +44,5 @@ document.write('<script src="rph-science-year3-future-blueprint-hotfix.js?v=2026
 document.write('<script src="rph-english-year2-unit5-source-blueprint-hotfix.js?v=20260905a"><\/script>');
 document.write('<script src="rph-english-year2-unit6-source-blueprint-hotfix.js?v=20260905a"><\/script>');
 document.write('<script src="rph-english-year2-unit7-source-blueprint-hotfix.js?v=20260905a"><\/script>');
+document.write('<script src="rph-english-year2-unit8-source-blueprint-hotfix.js?v=20260905a"><\/script>');
 document.write('<script src="rph-english-quality-hotfix.js?v=20260904a"><\/script>');
