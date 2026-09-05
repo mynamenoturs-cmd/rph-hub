ec126dedace10e2af2a187e73554b694dca049f8
mynamenoturs-cmd <mynamenoturs@gmail.com>
2026-09-05 10:56:01 +0800

    chore: load English Year 2 Unit 6 source blueprint

--- app-v03334.js
+++ app-v03334.js
@@ -42,4 +42,5 @@ document.write('<script src="rph-science-year2-technology-blueprint-hotfix.js?v=
 document.write('<script src="rph-science-year3-source-blueprint-hotfix.js?v=20260904a"><\/script>');
 document.write('<script src="rph-science-year3-future-blueprint-hotfix.js?v=20260904a"><\/script>');
 document.write('<script src="rph-english-year2-unit5-source-blueprint-hotfix.js?v=20260905a"><\/script>');
+document.write('<script src="rph-english-year2-unit6-source-blueprint-hotfix.js?v=20260905a"><\/script>');
 document.write('<script src="rph-english-quality-hotfix.js?v=20260904a"><\/script>');
