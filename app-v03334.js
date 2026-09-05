deb238ecd10b1e0f82ca3cdf695664d33ca1a084
mynamenoturs-cmd <mynamenoturs@gmail.com>
2026-09-05 20:16:42 +0800

    chore: load Science Year 1 Unit 5 source blueprint

--- app-v03334.js
+++ app-v03334.js
@@ -45,6 +45,7 @@ document.write('<script src="rph-science-year1-unit1-source-blueprint-hotfix.js?
 document.write('<script src="rph-science-year1-w8-w12-source-blueprint-hotfix.js?v=20260905a"><\/script>');
 document.write('<script src="rph-science-year1-unit3-source-blueprint-hotfix.js?v=20260905a"><\/script>');
 document.write('<script src="rph-science-year1-unit4-source-blueprint-hotfix.js?v=20260905a"><\/script>');
+document.write('<script src="rph-science-year1-unit5-source-blueprint-hotfix.js?v=20260905a"><\/script>');
 document.write('<script src="rph-english-year2-unit5-source-blueprint-hotfix.js?v=20260905a"><\/script>');
 document.write('<script src="rph-english-year2-unit6-source-blueprint-hotfix.js?v=20260905a"><\/script>');
 document.write('<script src="rph-english-year2-unit7-source-blueprint-hotfix.js?v=20260905a"><\/script>');
