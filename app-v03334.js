4ed2f85472eadac389fe59dee42b0778701f1d4f
mynamenoturs-cmd <mynamenoturs@gmail.com>
2026-09-05 14:48:52 +0800

    chore: load English Year 3 Module 6 source blueprint

--- app-v03334.js
+++ app-v03334.js
@@ -57,4 +57,5 @@ document.write('<script src="rph-english-year3-module2-source-blueprint-hotfix.j
 document.write('<script src="rph-english-year3-module3-source-blueprint-hotfix.js?v=20260905a"><\/script>');
 document.write('<script src="rph-english-year3-module4-source-blueprint-hotfix.js?v=20260905a"><\/script>');
 document.write('<script src="rph-english-year3-module5-source-blueprint-hotfix.js?v=20260905a"><\/script>');
+document.write('<script src="rph-english-year3-module6-source-blueprint-hotfix.js?v=20260905a"><\/script>');
 document.write('<script src="rph-english-quality-hotfix.js?v=20260904a"><\/script>');
